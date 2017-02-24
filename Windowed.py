90305282998a43eafde3b49537d3aa6b2a481621
Roy Lowrance <roy.lowrance@gmail.com>
2017-02-23 21:00:58 -0500

    method sum() always returns a number

--- Windowed.py
+++ Windowed.py
@@ -1,7 +1,6 @@
 import collections
+import pdb
 import unittest
-
-from MaybeNumber import MaybeNumber
 
 
 class Windowed(object):
@@ -17,34 +16,33 @@ class Windowed(object):
         self.items.append(item)
 
     def sum(self):
-        if len(self.items) < self.window_size:
-            return MaybeNumber(None)
-        else:
-            sum = 0
-            for item in self.items:
-                sum += item
-            return MaybeNumber(sum)
+        'return sum of items in the window'
+        sum = 0
+        for item in self.items:
+            sum += item
+        return sum
 
 
 class TestWindowed(unittest.TestCase):
     def test_window_size_1(self):
         w = Windowed(1)
         tests = (
-            (None, None),
+            (None, 0),
             (1, 1),
             (2, 2),
+            (3, 3),
         )
         for test in tests:
             item, expected_sum = test
             if item is not None:
                 w.append(item)
-            self.assertEqual(MaybeNumber(expected_sum), w.sum())
+            self.assertEqual(expected_sum, w.sum())
 
     def test_window_size_2(self):
         w = Windowed(2)
         tests = (
-            (None, None),
-            (1, None),
+            (None, 0),
+            (1, 1),
             (2, 3),
             (3, 5),
         )
@@ -52,8 +50,10 @@ class TestWindowed(unittest.TestCase):
             item, expected_sum = test
             if item is not None:
                 w.append(item)
-            self.assertEqual(MaybeNumber(expected_sum), w.sum())
+            self.assertEqual(expected_sum, w.sum())
 
 
 if __name__ == '__main__':
     unittest.main()
+    if False:
+        pdb
