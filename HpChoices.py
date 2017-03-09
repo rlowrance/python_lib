618b09bdcae04b06e49d348156f972ca92376412
Roy Lowrance <roy.lowrance@gmail.com>
2017-03-09 13:56:10 -0500

    abstract class: iterate over HpSpec choices

---
+++ HpChoices.py
@@ -0,0 +1,21 @@
+from abc import ABCMeta, abstractmethod
+import unittest
+
+
+class HpChoices(object):
+    'iterated over HpSpec instances'
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def iteritems(self):
+        'yield sequence of HpSpec objects'
+        pass
+
+
+class TestHpChoices(unittest.TestCase):
+    def test_construction(self):
+        self.assertRaises(Exception, HpChoices, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
