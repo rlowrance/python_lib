381741198e3aa811dee99cd3f2e666bdab00a8a6
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-11 12:39:52 -0400

    add copyright notice

--- py.bat
+++ py.bat
@@ -1,3 +1,16 @@
+rem Copyright 2017 Roy E. Lowrance
+rem 
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem 
+rem  http://www.apache.org/licenses/LICENSE-2.0
+rem 
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on as "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing premission and
+rem limitation under the license.
 @echo off
 flake8 %1
 if %errorlevel% equ 0 python %*
